847c959f36a0e7b7a127edcf69ce7cf88dfde4d7
CuteCat-UwU <udayavengerdude@gmail.com>
2021-05-21 23:16:04 +0530

    1

--- MCPE autosprint.py
+++ MCPE autosprint.py
@@ -1,4 +1,12 @@
 import subprocess
+
+
 def run_out(command_list):
     a=subprocess.run(command_list,stdout=subprocess.PIPE).stdout.decode('utf-8')
     return a
+
+doit= run_out(["xdotool","search"])
+print('''----------------------------------------------------------
+------------------------------------------------------
+------------------------------------------------------''')
+print(doit)
