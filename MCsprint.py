eeb1d5e70fda7e857a168157e6abe6822aa925c4
CuteCat-UwU <udayavengerdude@gmail.com>
2021-05-22 00:01:55 +0530

    asdf

--- MCsprint.py
+++ MCsprint.py
@@ -1,13 +1,14 @@
 import subprocess
-
+import time
 
 def run_out(command_list):
     a=subprocess.run(command_list,stdout=subprocess.PIPE,).stdout.decode('utf-8')
     return a
-name=input("Enter title of app:")
 
+#name=input("Enter title of app:")
 
-doit= run_out(["ls"])
+time.sleep(6)
+doit= run_out(["xdotool","key","r"])
 print('''------------------------------------------------------
 ------------------------------------------------------''')
 print(doit)
