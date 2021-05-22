8d96e1a7398c304f43fdabcc418af6a225303a24
CuteCat-UwU <udayavengerdude@gmail.com>
2021-05-22 11:15:30 +0530

    Can work

--- MCsprint.py
+++ MCsprint.py
@@ -5,7 +5,12 @@ def run_out(command_list):
     a=subprocess.run(command_list,stdout=subprocess.PIPE,).stdout.decode('utf-8')
     return a
 
-def k_down():
+def dn(a):
+    subprocess.run(['xdotool', 'keyup', '--window', a, 'ctrl'])  #this line prevents ctrl from bugging out if u switch windows
+    subprocess.run(['xdotool', 'keydown', '--window', a, 'ctrl'])
+
+def up(a):
+    subprocess.run(['xdotool', 'keyup', '--window', a, 'ctrl'])
 
 #getting window id for minecraft
 x=input("after pressing enter open minecraft within 5 seconds")
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # mcbe-togglesprint
-Togglesprint for mcpe
+Togglesprint for MCPE launcher on linux made my christopherHX
+link:  https://github.com/ChristopherHX/linux-packaging-scripts
+
+prerequisites: python3  (uses time.sleep and subprocesses)
+               xdotool  (sudo apt install xdotool)
+
+This most probably won't be updated ever
