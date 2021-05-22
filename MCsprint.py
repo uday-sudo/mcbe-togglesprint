ae3f337853033cfb71e18e2a298828514fac049d
CuteCat-UwU <udayavengerdude@gmail.com>
2021-05-22 12:54:23 +0530

    e

--- MCsprint.py
+++ MCsprint.py
@@ -1,20 +1,20 @@
-import subprocess
+from subprocess import run, PIPE
 from time import sleep
 
 def run_out(command_list):
-    a=subprocess.run(command_list,stdout=subprocess.PIPE,).stdout.decode('utf-8')
+    a=run(command_list,stdout=PIPE,).stdout.decode('utf-8')
     return a
 
 def dn(a):
-    subprocess.run(['xdotool', 'keyup', '--window', a, 'ctrl'])  #this line prevents ctrl from bugging out if u switch windows
-    subprocess.run(['xdotool', 'keydown', '--window', a, 'ctrl'])
+    run(['xdotool', 'keyup', '--window', a, 'ctrl'])  #this line prevents ctrl from bugging out if u switch windows
+    run(['xdotool', 'keydown', '--window', a, 'ctrl'])
 
 def up(a):
-    subprocess.run(['xdotool', 'keyup', '--window', a, 'ctrl'])
+    run(['xdotool', 'keyup', '--window', a, 'ctrl'])
 
 #getting window id for minecraft
 x=input("after pressing enter open minecraft within 5 seconds")
-time.sleep(5)
+sleep(5)
 winkey=run_out(["xdotool","getactivewindow"])
 print("the window id for minecraft is:",winkey)
 
